8c6a9e9c3cf7261c60ca3f6ca577ea4e4525a596
Tobias K <6317548+thecalcaholic@users.noreply.github.com>
2023-05-16 17:24:13 +0200

    Add preview to Admin Settings page

--- src/Dashboard.vue
+++ src/Dashboard.vue
@@ -22,15 +22,15 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 
 <template>
 	<div id="external-portal-widget">
-		<div v-if="loading" class="icon icon-loading" />
+		<div v-if="loading" class="icon icon-loading"/>
 		<span v-else-if="number > 0">
 			<div v-for="item in content"
-				:key="item.id"
-				:class="{ smaller: content.length>4 && content.length < 7, smallest: content.length>6, maxsized: maxSize}">
+				 :key="item.id"
+				 :class="{ smaller: content.length>4 && content.length < 7, smallest: content.length>6, maxsized: maxSize}">
 				<a v-bind="{ target: item.sameWindow ? '' : '_blank' }" :href="item.url">
 					<img class="linkitem"
-						preserveAspectRatio="xMinYMin meet"
-						:src="item.icon">
+						 preserveAspectRatio="xMinYMin meet"
+						 :src="item.icon">
 					<span class="linkname">
 						{{ item.name }}
 					</span>
@@ -46,13 +46,12 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 <script>
 
 import axios from '@nextcloud/axios'
-import { generateOcsUrl, generateUrl } from '@nextcloud/router'
-import { translate as t } from '@nextcloud/l10n'
+import {generateOcsUrl, generateUrl} from '@nextcloud/router'
+import {translate as t} from '@nextcloud/l10n'
 
 export default {
 	name: 'Dashboard',
-	components: {
-	},
+	components: {},
 	props: {
 		title: {
 			type: String,
@@ -69,8 +68,7 @@ export default {
 			showFiles: false,
 		}
 	},
-	computed: {
-	},
+	computed: {},
 	beforeMount() {
 		this.getConfig()
 		this.getContent()
@@ -78,48 +76,62 @@ export default {
 	mounted() {
 	},
 	methods: {
-		getConfig() {
+		async getConfig() {
 			const url = generateUrl('/apps/externalportal/config')
-			axios.get(url).then((response) => {
+			try {
+				const response = await axios.get(url)
+				console.debug('"' + JSON.stringify(response.data) + '"')
 				this.extraWide = response.data.extraWide
 				this.maxSize = response.data.maxSize
 				this.showFiles = response.data.showFiles
-				console.debug('"' + JSON.stringify(response.data) + '"')
-			}).catch((error) => {
-				console.debug(error)
-			}).then(() => {
-				if (this.showFiles) {
-					const filesUrl = generateUrl('/apps/files')
-					const filesIconUrl = generateUrl('../apps/files/img/app.svg')
-					const filesLabel = t('files', 'Files')
-					this.content = [{ icon: filesIconUrl, url: filesUrl, name: filesLabel, sameWindow: true }].concat(this.content)
-					this.number = this.content.length
-				}
-				if (this.number > 6 && this.extraWide) {
-					document.getElementById('external-portal-widget').parentNode.parentNode.style.width = '400px'
-				}
-			})
+			} catch (e) {
+				console.log(e)
+			}
+
+			if (this.showFiles) {
+				const filesUrl = generateUrl('/apps/files')
+				const filesIconUrl = generateUrl('../apps/files/img/app.svg')
+				const filesLabel = t('files', 'Files')
+				this.content = [{
+					icon: filesIconUrl,
+					url: filesUrl,
+					name: filesLabel,
+					sameWindow: true
+				}].concat(this.content)
+				this.number = this.content.length
+			}
+			if (this.number > 6 && this.extraWide) {
+				document.getElementById('external-portal-widget').parentNode.parentNode.style.width = '400px'
+			}
 		},
 
-		getContent() {
+		async getContent() {
 			let url = generateOcsUrl('apps/external/api/v1', 2)
 			if (url.endsWith('/')) { // behaviour seems to have changed between 24 and 25
 				url = url.slice(0, -1)
 			}
-			axios.get(url).then((response) => {
+			try {
+				const response = await axios.get(url)
 				this.content = this.content.concat(response.data.ocs.data)
 				this.number = this.content.length
 				console.debug('"' + JSON.stringify(response.data) + '"')
 				console.debug('"' + JSON.stringify(this.content) + '"')
-			}).catch((error) => {
+			} catch (error) {
 				console.debug(error)
-			}).then(() => {
-				this.loading = false
-				if (this.number > 6 && this.extraWide) {
-					document.getElementById('external-portal-widget').parentNode.parentNode.style.width = '400px'
-				}
-			})
+			}
+			this.loading = false
+			if (this.number > 6 && this.extraWide) {
+				document.getElementById('external-portal-widget').parentNode.parentNode.style.width = '400px'
+			}
 		},
+		async reload() {
+			this.loading = true
+			document.getElementById('external-portal-widget').parentNode.parentNode.style.width = '320px'
+			this.content = []
+			this.number = 0
+			await this.getConfig()
+			await this.getContent()
+		}
 	},
 }
 </script>
@@ -139,32 +151,39 @@ div {
 
 	img {
 		padding: 0.5rem;
-    -webkit-filter: drop-shadow(5px 5px 5px #888);
-    filter: drop-shadow(5px 5px 5px #888);
+		-webkit-filter: drop-shadow(5px 5px 5px #888);
+		filter: drop-shadow(5px 5px 5px #888);
 	}
+
 	.linkname {
-	position: relative;
-	top: -0.75rem;
+		position: relative;
+		top: -0.75rem;
 	}
 }
+
 div.icon-loading {
 	margin: 1rem;
 }
+
 div.smaller {
 	width: 39%;
 }
+
 div.smallest {
 	width: 30%;
 }
+
 div.maxsized {
 	max-width: 64px;
 	margin: 0.5rem;
 }
+
 .linkitem {
 	width: 95%;
 	height: 95%;
 	margin-top: 0.5rem;
 }
+
 .linkitem:hover {
 	transition: transform .2s;
 	transform: scale(1.1);
